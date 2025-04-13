e57cbca7a895d8dd74ae7ed3965af7566da4dba5
Ivonne Micaela Colque Murillo <161766924+IvonneCM@users.noreply.github.com>
2025-04-13 18:20:02 -0400

    Ruteo para organizar mejor

---
+++ src/app/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+
+import DepartamentoDash from '../components/departamentoDash';
+
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<DepartamentoDash/>} />
+        <Route path="/departamento" element={<DepartamentoDash />} />
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
