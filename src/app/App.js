d7eccc50cff6f0edb6dcc47b58ccef6931756620
Alkima-hydra <135045526+Alkima-hydra@users.noreply.github.com>
2025-04-13 17:39:50 -0400

    barra lateralm acabadom

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,22 @@
-import { Routes, Route} from 'react-router-dom';
-// TO-DO Implementar transicion entre pantallas aqui posteriormente
+import { Routes, Route } from 'react-router-dom';
 import { HomePage, DepPage, ErrorPage, RubrosPage } from '../pages';
-
+import Lateralbar from '../components/barraLateral/lateralbar';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/departamental" element={<DepPage/>} />
-        <Route path="*" element={<ErrorPage/>} />
-        <Route path="/rubros" element={<RubrosPage />}/>
-      </Routes>
+    <div className={styles.app}>
+      <Lateralbar />
+      <main className={styles.content}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Departamental" element={<DepPage />} />
+          <Route path="/Rubros" element={<RubrosPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </main>
     </div>
   );
 }
+
 export default App;
