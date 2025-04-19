32acf1d5380f6d7c66fa094b3b614c6b369a6a2a
CaFePiMe <159438510+CaFePiMe@users.noreply.github.com>
2025-04-19 12:41:13 -0400

    ajuste seccion 2

--- src/pages/rubrosPage/seccion1/seccion.jsx
+++ src/pages/rubrosPage/seccion1/seccion.jsx
@@ -1,22 +1,51 @@
 import React from 'react';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import styles from '../rubrosPage.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@fontsource/montserrat/700.css";
+import Pastel from '../rubro/grafic/graficq/pastelS';
+
+const WIDTH = "20%";
+const HEIGHT = "100%";
+
+
 
 const Seccion2 = () => {
     return (
         <div style={{
-            height: "100vh", 
+            height: "75vh", 
             width: "100%",
             borderColor: 'black',
-            backgroundColor: "black",
+            backgroundColor: "#E9F5FE",
         }}>
-            <h1>asdasdasd</h1>
-            
-           
-        
-      
+            <div style={{height: "45%", width: "100%", display: "flex", flexDirection: "row", gap: "10px"}}>
+                <Pastel width={WIDTH} height={HEIGHT} title='La Paz'/>
+                <Pastel width={WIDTH} height={HEIGHT} title='Santa Cruz'/>
+                <Pastel width={WIDTH} height={HEIGHT} title='Cochabamba'/>
+                <Pastel width={WIDTH} height={HEIGHT} title='Oruro'/>
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', height: '100%' }}>
+                    <div className={styles.detalles} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <p style={{ fontWeight: 'bold' }}>500</p>
+                        <p>Empresas</p>
+                    </div><br />
+                    <div className={styles.detalles} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <p style={{ fontWeight: 'bold' }}>10%</p>
+                        <p>crecimiento</p>
+                    </div><br />
+                    <div className={styles.detalles} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <p style={{ fontWeight: 'bold' }}>200</p>
+                        <p>años</p>
+                    </div>
+                </div>
+            </div><br />
+            <div style={{height: "45%", width: "100%", display: "flex", flexDirection: "row", gap: "10px"}}>
+                <Pastel width={WIDTH} height={HEIGHT} title='Potosí'/>
+                <Pastel width={WIDTH} height={HEIGHT} title='Chuquisaca'/>
+                <Pastel width={WIDTH} height={HEIGHT} title='Tarija'/>
+                <Pastel width={WIDTH} height={HEIGHT} title='Beni'/>
+                <Pastel width={WIDTH} height={HEIGHT} title='Pando'/>
+            </div>
         </div>
     );
 };
