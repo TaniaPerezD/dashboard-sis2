d0ee55636e460d3d31ba273384bcb39a810e6197
Alkima-hydra <latinoignas@gmail.com>
2025-04-19 16:32:23 -0400

    antes de casita, cartita

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import styles from './homePage.module.css';
+import HomeCard from '../../components/home/HomeCard';
 
 const HomePage = () => {
+    const props = {
+        numero: '100',
+        texto: 'Total de Empresas',
+        porcentaje: '10%'
+    };
+
     return (
         <div>
             <h1 className={styles.h1}>Pagina de Bienvenida</h1>
+            <HomeCard 
+                numero={props.numero} 
+                texto={props.texto} 
+                porcentaje={props.porcentaje} 
+            />
         </div>
     );
 }; export default HomePage;
