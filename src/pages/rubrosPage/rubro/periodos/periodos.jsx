549e85610f67e5d9049dd18b792e8cc17f8ce5e8
KEENTAA <ramiro.quenta@ucb.edu.bo>
2025-04-19 23:37:58 -0400

    Descargue "rc-slider": "^11.1.8",

--- src/pages/rubrosPage/rubro/periodos/periodos.jsx
+++ src/pages/rubrosPage/rubro/periodos/periodos.jsx
@@ -11,18 +11,16 @@ const Periodos = ({ width = "15%", height = "10%" }) => {
         borderColor: '#E9F5FE',
         backgroundColor: "#E9F5FE",
         display: "flex",
-        alignItems: "center",
-        padding: 0,
+        justifyContent: "center",
+        gap: "4rem",
         boxShadow: "none",
-        position: "relative",
-        gap: "0.5rem",
         flexDirection: "row",
       }}
     >
-      <Periodo width="10%" height="100%"/>
-      <Periodo width="10%" height="100%"/>
-      <Periodo width="10%" height="100%"/>
-      <Periodo width="10%" height="100%"/>
+      <Periodo width="10%" height="100%" titulo='GRANDES E.' subtitulo='10%'/>
+      <Periodo width="10%" height="100%"titulo='MEDIANAS E.' subtitulo='30%'/>
+      <Periodo width="10%" height="100%"titulo='PEQUEÑAS E.' subtitulo='60%'/>
+      
       <Filtro width="10%" height="100%"/>
     </div>
   );
