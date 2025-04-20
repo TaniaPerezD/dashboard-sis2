8222f899bb77d8a593cc8d7ed56de009222e69d7
Tania Morelia Pérez Dick <160441663+TaniaPerezD@users.noreply.github.com>
2025-04-20 11:18:40 -0400

    cambio para coordinar

--- src/pages/seccionDep/seccionTresDep.jsx
+++ src/pages/seccionDep/seccionTresDep.jsx
@@ -3,17 +3,16 @@ import React, { useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import '../../styles/departamento.css';
 import { useNavigate } from 'react-router-dom';
-import {
-    BarChart, Bar, XAxis, YAxis, CartesianGrid,
-    Tooltip, Legend, ResponsiveContainer, LabelList
-} from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line, LabelList } from 'recharts';
 
 import empresas from '../../data/departamento/dataEmpresas.json';
 
 const SeccionTresPage = () => {
-    const navigate = useNavigate();
-    const [departamentoSeleccionado, setDepartamentoSeleccionado] = useState(null);
+    
+    const navigate = useNavigate(); //para la navegacion entre secciones
 
+    const [departamentoSeleccionado, setDepartamentoSeleccionado] = useState(null);
+    
     const departamentoListados = () => {
         const departamentosSet = new Set();
         empresas.forEach((empresa) => {
@@ -24,54 +23,85 @@ const SeccionTresPage = () => {
         return Array.from(departamentosSet);
     };
 
-    const procesoGraficoActivo = () => {
+    const procesoGraficoActivo = (departamentoSeleccionado) => {
         const resumen = {};
+        let flag = false;
+
+        if (departamentoSeleccionado === null) {
+            flag = true;
+        }
 
         empresas.forEach((empresa) => {
-            if (departamentoSeleccionado && empresa.departamento !== departamentoSeleccionado) return;
+            if (empresa.departamento !== departamentoSeleccionado && !flag) return;
 
             const departamento = empresa.departamento;
+            const activo = empresa.activo;
+
             if (!resumen[departamento]) {
-                resumen[departamento] = { departamento, activa: 0, inactiva: 0 };
+                resumen[departamento] = { departamento, activo: 0, inactivo: 0 };
             }
 
-            if (empresa.activa) {
-                resumen[departamento].activa += 1;
-            } else {
-                resumen[departamento].inactiva += 1;
-            }
+                if(activo) {
+                    resumen[departamento].activo += 1;
+                }
+                else {
+                    resumen[departamento].inactivo += 1;
+                }
         });
 
         return Object.values(resumen);
     };
 
-    const procesoGraficoMes = () => {
+    const procesoGraficoMes = (departamentoSeleccionado) => {
+
         const ordenMeses = [
             "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
             "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
         ];
 
         const resumen = {};
+        let flag = false;
+
+        if (departamentoSeleccionado === null) {
+            flag = true;
+        }
 
         empresas.forEach((empresa) => {
-            if (departamentoSeleccionado && empresa.departamento !== departamentoSeleccionado) return;
+            if (empresa.departamento !== departamentoSeleccionado && !flag) return;
 
-            const apertura = empresa.mes_apertura;
-            const cierre = empresa.mes_cierre;
+            const mesApertura = empresa.mes_apertura;
+            const mesCierre = empresa.mes_cierre;
 
-            if (apertura) {
-                if (!resumen[apertura]) resumen[apertura] = { mes: apertura, apertura: 0, cierre: 0 };
-                resumen[apertura].apertura++;
+            if (!resumen[mesApertura]) {
+            resumen[mesApertura] = { mes: mesApertura, apertura: 0, cierre: 0 };
             }
+            resumen[mesApertura].apertura++;
 
-            if (cierre) {
-                if (!resumen[cierre]) resumen[cierre] = { mes: cierre, apertura: 0, cierre: 0 };
-                resumen[cierre].cierre++;
+            if (mesCierre) {
+            if (!resumen[mesCierre]) {
+                resumen[mesCierre] = { mes: mesCierre, apertura: 0, cierre: 0 };
             }
+            resumen[mesCierre].cierre++;
+            }
+            
         });
-
-        return ordenMeses.map(mes => resumen[mes] || { mes, apertura: 0, cierre: 0 });
+        
+        const resumenOrdenado = Object.values(resumen).sort((a, b) => {
+            return ordenMeses.indexOf(a.mes) - ordenMeses.indexOf(b.mes);
+        });
+        
+        return resumenOrdenado;
     };
+
+    const ordenarMeses = (resumen) => {
+        const ordenMeses = [
+            "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+            "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        ];
+        resumen.sort((a, b) => {
+            return ordenMeses.indexOf(a.mes) - ordenMeses.indexOf(b.mes);
+        });
+    }
 
     return (
         <div className="container">
@@ -82,106 +112,142 @@ const SeccionTresPage = () => {
                 <div className="col-1">
                     <Dropdown>
                         <Dropdown.Toggle id="dropdown-basic" className="dropdown-custom">
-                            Sección 3
+                            Secciones
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
-                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionUno')}>Sección 1</Dropdown.Item>
-                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionDos')}>Sección 2</Dropdown.Item>
-                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionTres')}>Sección 3</Dropdown.Item>
-                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionCuatro')}>Sección 4</Dropdown.Item>
+                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionUno')}>
+                                Seccion 1</Dropdown.Item>
+                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionDos')}>
+                                Seccion 2</Dropdown.Item>
+                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionTres')}>
+                                Seccion 3</Dropdown.Item>
+                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionCuatro')}>
+                                Seccion 4</Dropdown.Item>
                         </Dropdown.Menu>
-                    </Dropdown>
+                    </Dropdown>                    
                 </div>
             </div>
-
+            
             <div className="row">
                 <div className="col-4">
-                    <div className="card-dashboard" style={{ height: '90px', marginBottom: '20px' }}>
-                        <div className="card-dashboard-header">Filtrar por Departamento</div>
-                        <div className="card-dashboard-content">
+                    <div className="row">
+                        <div className="card-dashboard" style={{ height: '80px'}}>
+                            <div className="card-dashboard-header">
+                                Departamento 
+                            </div>
                             <div className="d-flex justify-content-between align-items-center gap-2 mt-1 px-3 mb-3">
+                                 <Dropdown>
+                                     <Dropdown.Toggle
+                                         id="dropdown-basic"
+                                         style={{
+                                             backgroundColor: 'white',
+                                             color: '#182335',
+                                             border: '1px solid #182335'
+                                         }}
+                                     >
+                                         {departamentoSeleccionado || "Departamento"}
+                                     </Dropdown.Toggle>
+                                     <Dropdown.Menu>
+                                         {departamentoListados().map(dep => (
+                                             <Dropdown.Item key={dep} onClick={() => setDepartamentoSeleccionado(dep)}>
+                                                 {dep}
+                                             </Dropdown.Item>
+                                         ))}
+                                     </Dropdown.Menu>
+                                 </Dropdown>
+ 
+                                 <button
+                                     className="btn btn-outline-secondary btn-sm"
+                                     onClick={() => setDepartamentoSeleccionado(null)}
+                                 >
+                                     Limpiar filtros
+                                 </button>
+                             </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="card-dashboard" style={{ height: '60vh'}}>
+                            <div className="card-dashboard-header">
+                                Q Aperturas y cierres por mes
                                 <Dropdown>
-                                    <Dropdown.Toggle
-                                        id="dropdown-basic"
-                                        style={{
-                                            backgroundColor: 'white',
-                                            color: '#182335',
-                                            border: '1px solid #182335'
-                                        }}
-                                    >
-                                        {departamentoSeleccionado || "Departamento"}
+                                    <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                                        Departamento
                                     </Dropdown.Toggle>
                                     <Dropdown.Menu>
-                                        {departamentoListados().map(dep => (
-                                            <Dropdown.Item key={dep} onClick={() => setDepartamentoSeleccionado(dep)}>
-                                                {dep}
+                                        {departamentoListados().map((departamento) => (
+                                            <Dropdown.Item
+                                                key={departamento}
+                                                onClick={() => setDepartamentoSeleccionado(departamento)}
+                                            >
+                                                {departamento}
                                             </Dropdown.Item>
                                         ))}
                                     </Dropdown.Menu>
                                 </Dropdown>
-
-                                <button
-                                    className="btn btn-outline-secondary btn-sm"
-                                    onClick={() => setDepartamentoSeleccionado(null)}
-                                >
-                                    Limpiar filtros
-                                </button>
                             </div>
-                        </div>
-                    </div>
-
-                    <div className="card-dashboard" style={{ height: '60vh' }}>
-                        <div className="card-dashboard-header">📊 Aperturas y cierres por mes</div>
-                        <div className="card-dashboard-content">
-                            <ResponsiveContainer width="100%" height="100%">
-                                <BarChart layout="vertical" data={procesoGraficoMes()}>
+                            <div className="card-dashboard-content">
+                                <ResponsiveContainer width="100%" height="100%">
+                                    <BarChart
+                                    layout="vertical" 
+                                    data={procesoGraficoMes(departamentoSeleccionado)}
+                                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                                    >
                                     <CartesianGrid strokeDasharray="3 3" />
                                     <XAxis type="number" />
                                     <YAxis dataKey="mes" type="category" />
                                     <Tooltip />
                                     <Legend />
-                                    <Bar dataKey="apertura" fill="#EEAF9D">
-                                        <LabelList dataKey="apertura" position="right" formatter={(value) => value} />
-                                    </Bar>
-                                    <Bar dataKey="cierre" fill="#94A3BA">
-                                        <LabelList dataKey="cierre" position="right" formatter={(value) => value} />
-                                    </Bar>
-                                </BarChart>
-                            </ResponsiveContainer>
+                                    <Bar dataKey="apertura" fill="#EEAF9D" />
+                                    <Bar dataKey="cierre" fill="#94A3BA" />
+                                    </BarChart>
+                                </ResponsiveContainer>
+                            </div>
                         </div>
                     </div>
                 </div>
-
                 <div className="col-4">
-                    <div className="card-dashboard" style={{ height: '75vh' }}>
-                        <div className="card-dashboard-header">🗺️ Ubicación</div>
-                        <div className="card-dashboard-content d-flex justify-content-center align-items-center" style={{ height: '100%' }}>
-                            <img
-                                src="https://i.pinimg.com/736x/86/bf/2f/86bf2fd1cef4d4e137d70eab4d0c8cbb.jpg"
-                                alt="Mapa de Bolivia"
-                                style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-                            />
+                    <div className="card-dashboard" style={{ height: '70vh'}}>
+                        <div className="card-dashboard-header">Departamento</div>
+                        <div className="card-dashboard-content"   style={{ height: '50vh'}}>
+                            <img src="https://i.pinimg.com/736x/86/bf/2f/86bf2fd1cef4d4e137d70eab4d0c8cbb.jpg" alt="" />
+                            
                         </div>
                     </div>
                 </div>
-
                 <div className="col-4">
-                    <div className="card-dashboard" style={{ height: '70vh' }}>
-                        <div className="card-dashboard-header">📌 Activas vs Inactivas</div>
+                    <div className="card-dashboard" style={{ height: '70vh'}}>
+                        <div className="card-dashboard-header">
+                            Q empresas activas vs inactivas
+                            <Dropdown>
+                                <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                                    Departamento
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    {departamentoListados().map((departamento) => (
+                                        <Dropdown.Item
+                                            key={departamento}
+                                            onClick={() => setDepartamentoSeleccionado(departamento)}
+                                        >
+                                            {departamento}
+                                        </Dropdown.Item>
+                                    ))}
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
                         <div className="card-dashboard-content">
-                            <ResponsiveContainer width="100%" height="100%">
-                                <BarChart layout="vertical" data={procesoGraficoActivo()}>
-                                    <CartesianGrid strokeDasharray="3 3" />
-                                    <XAxis type="number" />
-                                    <YAxis dataKey="departamento" type="category" />
-                                    <Tooltip />
-                                    <Legend />
-                                    <Bar dataKey="activa" fill="#2a9d8f">
-                                        <LabelList dataKey="activa" position="right" formatter={(value) => value} />
-                                    </Bar>
-                                    <Bar dataKey="inactiva" fill="#e76f51">
-                                        <LabelList dataKey="inactiva" position="right" formatter={(value) => value} />
-                                    </Bar>
+                            <ResponsiveContainer width=" 100%" height="100%">
+                                <BarChart
+                                layout="vertical" 
+                                data={procesoGraficoActivo(departamentoSeleccionado)}
+                                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                                >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis type="number" />
+                                <YAxis dataKey="departamento" type="category" />
+                                <Tooltip />
+                                <Legend />
+                                <Bar dataKey="activo" fill="#EEAF9D" />
+                                <Bar dataKey="inactivo" fill="#94A3BA" />
                                 </BarChart>
                             </ResponsiveContainer>
                         </div>
@@ -190,6 +256,6 @@ const SeccionTresPage = () => {
             </div>
         </div>
     );
-};
+}
 
 export default SeccionTresPage;
