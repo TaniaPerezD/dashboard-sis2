79cc86f2c21590c4a9db30076cafded49bb69161
Tania Morelia Pérez Dick <160441663+TaniaPerezD@users.noreply.github.com>
2025-04-20 11:54:17 -0400

    cambios

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
-import DepPage from '../pages/departamentosPage/index';
 import { Routes, Route } from 'react-router-dom';
-import { HomePage, ErrorPage, RubrosPage, SeccionDosPage, SeccionTresPage, SeccionCuatroPage
+import { HomePage, ErrorPage, RubrosPage, 
+  SeccionUnoPage, SeccionDosPage, SeccionTresPage, SeccionCuatroPage
  } from '../pages';
 import Lateralbar from '../components/barraLateral/lateralbar';
 import styles from './App.module.css';
@@ -14,10 +14,6 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/Departamental" element={<SeccionUnoPage />} />
           <Route path="/Rubros" element={<RubrosPage />} />
-          <Route path="/Departamental/SeccionUno" element={<DepPage />} />
-          <Route path="/Departamental/SeccionDos" element={<SeccionDosPage />} />
-          <Route path="/Departamental/SeccionTres" element={<SeccionTresPage />} />
-          <Route path="/Departamental/SeccionCuatro" element={<SeccionCuatroPage />} />
           <Route path="/Departamental/SeccionUno" element={<SeccionUnoPage />} />
           <Route path="/Departamental/SeccionDos" element={<SeccionDosPage />} />
           <Route path="/Departamental/SeccionTres" element={<SeccionTresPage />} />
