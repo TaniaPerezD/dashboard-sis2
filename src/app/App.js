fb99c4500d605a531cd1884f793e6754a3fc01c2
Tania Morelia Pérez Dick <160441663+TaniaPerezD@users.noreply.github.com>
2025-04-20 11:16:39 -0400

    app y data

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
+import DepPage from '../pages/departamentosPage/index';
 import { Routes, Route } from 'react-router-dom';
-import { HomePage, ErrorPage, RubrosPage, 
-  SeccionUnoPage, SeccionDosPage, SeccionTresPage, SeccionCuatroPage
+import { HomePage, ErrorPage, RubrosPage, SeccionDosPage, SeccionTresPage, SeccionCuatroPage
  } from '../pages';
 import Lateralbar from '../components/barraLateral/lateralbar';
 import styles from './App.module.css';
@@ -12,9 +12,9 @@ function App() {
       <main className={styles.content}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/Departamental" element={<SeccionUnoPage />} />
+          <Route path="/Departamental" element={<DepPage />} />
           <Route path="/Rubros" element={<RubrosPage />} />
-          <Route path="/Departamental/SeccionUno" element={<SeccionUnoPage />} />
+          <Route path="/Departamental/SeccionUno" element={<DepPage />} />
           <Route path="/Departamental/SeccionDos" element={<SeccionDosPage />} />
           <Route path="/Departamental/SeccionTres" element={<SeccionTresPage />} />
           <Route path="/Departamental/SeccionCuatro" element={<SeccionCuatroPage />} />
