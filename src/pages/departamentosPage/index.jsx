28bc499c0b338a7e4f42a0638ce64f1970f480b6
Ivonne Micaela Colque Murillo <161766924+IvonneCM@users.noreply.github.com>
2025-04-19 20:33:20 -0400

    Routeo de secciones

--- src/pages/departamentosPage/index.jsx
+++ src/pages/departamentosPage/index.jsx
@@ -3,12 +3,16 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import '../../styles/departamento.css';
 import React, { useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line, LabelList } from 'recharts';
+import { useNavigate } from 'react-router-dom';
 
 
 
 import empresas from '../../data/departamento/dataEmpresas.json';
 
 const DepPage = () => {
+
+    const navigate = useNavigate(); //para la navegacion entre secciones
+
     const [departamentoSeleccionado, setDepartamentoSeleccionado] = useState(null);
 
     // Datos en trajetas
@@ -120,7 +124,26 @@ const DepPage = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="titulos">DEPARTAMENTOS</div>
+                <div className="col-11">
+                    <div className="titulos">DEPARTAMENTOS</div>
+                </div>
+                <div className="col-1">
+                    <Dropdown>
+                        <Dropdown.Toggle id="dropdown-basic" className="dropdown-custom">
+                            Secciones
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionUno')}>
+                                Seccion 1</Dropdown.Item>
+                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionDos')}>
+                                Seccion 2</Dropdown.Item>
+                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionTres')}>
+                                Seccion 3</Dropdown.Item>
+                            <Dropdown.Item onClick={() => navigate('/Departamental/SeccionCuatro')}>
+                                Seccion 4</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>                    
+                </div>
             </div>
 
             <div className="row">
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,9 @@ import DepPage from "./departamentosPage";
 import ErrorPage from "./errorPage";
 import HomePage from "./homePage";
 import RubrosPage from "./rubrosPage";
+import SeccionUnoPage from "./seccionDep/seccionUnoDep";
+import SeccionDosPage from "./seccionDep/seccionDosDep";
+import SeccionTresPage from "./seccionDep/seccionTresDep";
+import SeccionCuatroPage from "./seccionDep/seccionCuatroDep";
 
-export { DepPage, ErrorPage, HomePage, RubrosPage };
+export { DepPage, ErrorPage, HomePage, RubrosPage , SeccionUnoPage, SeccionDosPage, SeccionTresPage, SeccionCuatroPage };
