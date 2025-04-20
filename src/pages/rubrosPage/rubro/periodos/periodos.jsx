2148862f13d894348871ebd4e68b73b31691caf3
KEENTAA <ramiro.quenta@ucb.edu.bo>
2025-04-20 05:14:01 -0400

    Update periodos.jsx

--- src/pages/rubrosPage/rubro/periodos/periodos.jsx
+++ src/pages/rubrosPage/rubro/periodos/periodos.jsx
@@ -15,12 +15,12 @@ const Periodos = ({ width = "15%", height = "10%" }) => {
         gap: "3.5rem",
         boxShadow: "none",
         flexDirection: "row",
-        margin: "0px 105px"
+        margin: "0px 80px"
       }}
     >
-      <Periodo width="10%" height="100%" titulo='GRANDES E.' subtitulo='11.5% (149)'/>
-      <Periodo width="10%" height="100%"titulo='MEDIANAS E.' subtitulo='32.7% (422)'/>
-      <Periodo width="10%" height="100%"titulo='PEQUEÑAS E.' subtitulo='55.8% (1346)'/>
+      <Periodo width="15%" height="100%" titulo='GRANDES EMPRESAS.' subtitulo='11.5% (149)'/>
+      <Periodo width="15%" height="100%"titulo='MEDIANAS EMPRESAS.' subtitulo='32.7% (422)'/>
+      <Periodo width="15%" height="100%"titulo='EMPRESAS FAMILIARES.' subtitulo='55.8% (1346)'/>
       
       <Filtro width="10%" height="100%"/>
     </div>
