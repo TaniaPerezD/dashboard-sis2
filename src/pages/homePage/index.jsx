ea87e79d57413bd81adcd616a1a52d857afb438e
Tania Morelia Pérez Dick <160441663+TaniaPerezD@users.noreply.github.com>
2025-05-29 01:07:14 -0400

    wola?

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './homePage.module.css';
 import HomeCard from '../../components/home/HomeCard';
 
 const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className={styles.loaderContainer}>
+        <div className={styles.spinner}></div>
+      </div>
+    );
+  }
+
   const cardUrls = [
-    'http://meta-tania.serverbb.site/public/question/46f330eb-5b1b-4074-8f6e-6e746d30cb2c',
-    'http://meta-tania.serverbb.site/public/question/5e6ae2b3-bb1c-4d56-bad0-81f13f88e9b9',
-    'http://meta-tania.serverbb.site/public/question/98190f94-2c5d-41f0-bebc-6f0a4e586b9d',
-    'http://meta-tania.serverbb.site/public/question/7464fa09-7a7c-4028-ab9e-1a9019e7addd',
+    'https://meta-tania.serverbb.site/public/question/46f330eb-5b1b-4074-8f6e-6e746d30cb2c',
+    'https://meta-tania.serverbb.site/public/question/5e6ae2b3-bb1c-4d56-bad0-81f13f88e9b9',
+    'https://meta-tania.serverbb.site/public/question/98190f94-2c5d-41f0-bebc-6f0a4e586b9d',
+    'https://meta-tania.serverbb.site/public/question/7464fa09-7a7c-4028-ab9e-1a9019e7addd',
   ];
 
   const chartUrls = [
-    'http://meta-tania.serverbb.site/public/question/c59cbaaf-65b2-433a-a56f-727f628eac59',
-    'http://meta-tania.serverbb.site/public/question/a3e0fa41-ce23-419d-9179-7245d3c8d567',
+    'https://meta-tania.serverbb.site/public/question/c59cbaaf-65b2-433a-a56f-727f628eac59',
+    'https://meta-tania.serverbb.site/public/question/a3e0fa41-ce23-419d-9179-7245d3c8d567',
   ];
 
-  const mapUrl = 'http://meta-tania.serverbb.site/public/question/9f6330ce-d954-4afd-8f4f-1865c3293697';
+  const mapUrl = 'https://meta-tania.serverbb.site/public/question/9f6330ce-d954-4afd-8f4f-1865c3293697';
 
   return (
     <div className={styles.container}>
